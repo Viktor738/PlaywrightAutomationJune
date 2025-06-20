e54f6cd46677bb5dc0e6e4ba5575d369962de684
Viktor738 <viktor05061984@gmail.com>
2025-06-19 23:37:36 -0400

    changed command

--- global-setup.ts
+++ global-setup.ts
@@ -1,7 +1,7 @@
 import { chromium, FullConfig } from '@playwright/test';
 
 async function globalSetup(config: FullConfig) {
-  const browser = await chromium.launch({headless:false});
+  const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
 
